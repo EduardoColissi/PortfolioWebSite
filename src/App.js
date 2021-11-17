fa2af0a2301d7cae89199df1de816fc8f6068440
EduardoColissi <educolissi@hotmail.com>
2021-11-16 22:03:36 -0300

    feat: added default bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/organisms/Navbar";
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Navbar />
   );
 }
 
